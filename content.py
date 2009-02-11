587ca73ba8db3cd36117f9011dbd8a6de393c5e5
adam <adam@pkqk.net>
2009-02-12 11:07:28 +1300

    fixup template, error handling, and index path

--- content.py
+++ content.py
@@ -10,18 +10,22 @@ class StaticPublisher:
     self.environ = environ
     try:
       with open(self.path_from_request()) as doc:
-        values = {'title': 'test title'}
-        values['body'] = doc.read() + "\n" + self.show_environ()
+        values = {'title': 'test title', 'body': doc.read()}
         response = template.standard(values)
         start_response('200 OK', [('Content-Type','text/html;charset=utf-8')])
         return response
-    except Exception as e:
+    except IOError as e:
       start_response('404 Not Found', [('Content-Type', 'text/plain')])
+      return template.not_found(e)
+    except Exception as e:
+      start_response('500 Internal Server Error', [('Content-Type', 'text/plain')])
       return template.error(e)
 
   def show_environ(self):
     return "<dl>%s</dl>" % "\n".join("<dt>%s</dt><dd>%s</dd>" % (k,v) for k,v in self.environ.items())
 
   def path_from_request(self):
-    path = "%s.doc" % os.path.normpath(self.environ.get('PATH_INFO'))[1:]
-    return os.path.join(self.doc_root,path)
+    path = os.path.normpath(self.environ.get('PATH_INFO'))[1:]
+    if path == '':
+      path = 'index'
+    return os.path.join(self.doc_root, '%s.doc' % path)
--- template.py
+++ template.py
@@ -1,20 +1,35 @@
 import traceback
 
 def standard(values):
-  layout = """<!doctype html>
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
-<title>%(title)s</title>
-</head>
-<body>
-%(body)s
-</body>
-</html>"""
-  return [layout % values]
+  return ["\n".join(
+    doctype(values) +
+    surround('html',
+      surround('head',title(values)+stylesheet(values)) +
+      body(values)
+    )
+  )]
+
+def title(values):
+  return surround('title',values['title'])
   
-def meta(extra=[]):
-  return """<link rel="stylesheet" href="/style/base.css" type="text/css" media="all" />"""
+def doctype(values):
+  return ['<!doctype html>']
+  
+def stylesheet(values):
+  return ['<link rel="stylesheet" href="/style/base.css" type="text/css" media="all" />']
+
+def body(values):
+  return surround('body',values['body'])
+  
+def surround(tag,content):
+  if isinstance(content,str):
+    inner = [content]
+  else:
+    inner = content
+  return ['<%s>'%tag] + inner + ['</%s>'%tag]
 
 def error(exception):
-  return [str(exception),"\n"] + traceback.format_tb(exception.__traceback__)
+  return ["\n".join([repr(exception),str(exception)]+traceback.format_tb(exception.__traceback__))]
+
+def not_found(exception):
+  return ["resource not found: %s" % exception.filename]
