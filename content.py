710ccfd1e2f53d00e066c7eab0f08518afd48a36
adam <adam@pkqk.net>
2009-02-13 11:48:48 +1300

    refactor template to collect environ, attrs etc

--- content.py
+++ content.py
@@ -12,7 +12,7 @@ class StaticPublisher:
     try:
       with open(self.path_from_request()) as doc:
         attrs, content = self.load_document(doc.read())
-        response = template.standard(attrs, content)
+        response = template.standard(environ, attrs, content)
         start_response('200 OK', [('Content-Type','text/html;charset=utf-8')])
         return response
     except IOError as e:
@@ -21,9 +21,6 @@ class StaticPublisher:
     except Exception as e:
       start_response('500 Internal Server Error', [('Content-Type', 'text/plain')])
       return template.error(e)
-
-  def show_environ(self):
-    return "<dl>%s</dl>" % "\n".join("<dt>%s</dt><dd>%s</dd>" % (k,v) for k,v in self.environ.items())
 
   def path_from_request(self):
     path = os.path.normpath(self.environ.get('PATH_INFO'))[1:]
--- template.py
+++ template.py
@@ -1,32 +1,45 @@
 import traceback
 
-def standard(attrs, content):
-  return ["\n".join(
-    prelude(attrs) +
-    surround('html',
-      surround('head',title(attrs)+stylesheet(attrs)) +
-      body(attrs, content)
-    )
-  )]
+def standard(environ, attrs, content):
+  return StandardTemplate(environ,attrs,content).render()
 
-def title(attrs):
-  return surround('title',attrs['title'])
-  
-def prelude(attrs):
-  return ['<!doctype html>']
-  
-def stylesheet(attrs):
-  return ['<link rel="stylesheet" href="/style/base.css" type="text/css" media="all" />']
+class StandardTemplate:
+  def __init__(self,environ,attrs,content):
+    self.environ = environ
+    self.attrs = attrs
+    self.content = content
 
-def body(attrs, content):
-  return surround('body', content)
+  def title(self):
+    return self.surround('title',self.attrs['title'])
   
-def surround(tag,content):
-  if isinstance(content,str):
-    inner = [content]
-  else:
-    inner = content
-  return ['<%s>'%tag] + inner + ['</%s>'%tag]
+  def prelude(self):
+    return ['<!doctype html>']
+  
+  def stylesheet(self):
+    return ['<link rel="stylesheet" href="/style/base.css" type="text/css" media="all" />']
+
+  def body(self):
+    return self.surround('body', self.content)
+    # return self.surround('body', self.content + self.show_environ())
+  
+  def surround(self,tag,content):
+    if isinstance(content,str):
+      inner = [content]
+    else:
+      inner = content
+    return ['<%s>'%tag] + inner + ['</%s>'%tag]
+    
+  def show_environ(self):
+    return "<dl>%s</dl>" % "\n".join("<dt>%s</dt><dd>%s</dd>" % (k,v) for k,v in self.environ.items())
+  
+  def render(self):
+    return ["\n".join(
+      self.prelude() +
+      self.surround('html',
+        self.surround('head',self.title()+self.stylesheet()) +
+        self.body()
+      )
+    )]
 
 def error(exception):
   return ["\n".join([repr(exception),str(exception)]+traceback.format_tb(exception.__traceback__))]
